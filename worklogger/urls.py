2c4bd1b6d545e572e5fc3b49e19b4ed402622bd6
Alexandr Zakharov <sasha750i@mail.ru>
2017-05-22 16:51:37 +0300

    Static files. Index page for angular.

--- worklogger/urls.py
+++ worklogger/urls.py
@@ -1,6 +1,17 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+
+from worklog.views import HomePage
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, settings.MEDIA_ROOT)
+
+urlpatterns += [
+    url(r'', HomePage.as_view(), name='home'),
 ]
