c33b006555ea7ecadb002262601e1d988f2e5a7a
Alexandr Zakharov <sasha750i@mail.ru>
2017-05-22 15:25:42 +0300

    Initial commit/Backend LogEntry

---
+++ worklog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from worklog.models import LogEntry
+
+admin.site.register(LogEntry)
---
+++ worklog/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class LogEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='entries')
+    date = models.DateField()
+    late_log = models.BooleanField(default=False)
+    project = models.CharField(max_length=150)
+    remarks = models.CharField(max_length=150)
+    duration = models.DurationField()
+
+    class Meta:
+        verbose_name_plural = 'Log entries'
